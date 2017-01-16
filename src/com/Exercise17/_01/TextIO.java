e506ccb3c5a00f7b3407d59a07d17277bd30e717
RedPhanthom <RedPhanthom@outlook.com>
2017-01-16 15:31:58 -0500

    Code Addition

--- src/com/Exercise17/_01/TextIO.java
+++ src/com/Exercise17/_01/TextIO.java
@@ -1,5 +1,33 @@
+/*--------------------------------------------------------------------
+Name: Jaime Palencia
+Student ID: U94665425
+COP 2805C - Java Programming 2
+Spring /2017
+Assignment # 1
+Plagiarism Statement
+I certify that this assignment is my own work and that I
+have not copied in part or whole or otherwise plagiarized 
+the work of other students and/or persons.
+--------------------------------------------------------------------*/
 package com.Exercise17._01;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Random;
 
 public class TextIO {
 
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		try {
+			PrintWriter pw = new PrintWriter(new FileOutputStream("Exercise17-01.txt"), true);
+			for (int i = 0; i < 100; i++) {
+				pw.print(rnd.nextInt(100) + "\n");
+			}
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/com/Exercise17/_02/BinaryIO.java
+++ src/com/Exercise17/_02/BinaryIO.java
@@ -1,5 +1,19 @@
+/*--------------------------------------------------------------------
+Name: Jaime Palencia
+Student ID: U94665425
+COP 2805C - Java Programming 2
+Spring /2017
+Assignment # 1
+Plagiarism Statement
+I certify that this assignment is my own work and that I
+have not copied in part or whole or otherwise plagiarized 
+the work of other students and/or persons.
+--------------------------------------------------------------------*/
 package com.Exercise17._02;
 
 public class BinaryIO {
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
